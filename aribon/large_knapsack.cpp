08b4ef6c2b39d971c5b44a10a19c953be4b1e931
trmr <ryo1.teramura@gmail.com>
2020-03-15 00:47:28 +0900

    large_knapsack

--- aribon/large_knapsack.cpp
+++ aribon/large_knapsack.cpp
@@ -4,15 +4,69 @@ using namespace std;
 
 using ll = long long;
 
-int main() {
-    int N; cin >> N;
-    vector<ll> w(N), v(N);
-    ll W;
+const int MAX_N = 10000;
+const ll INF = 10000000;
 
-    for (auto& x:w) cin >> x;
-    for (auto& x:v) cin >> x;
+int n;
+ll w[MAX_N], v[MAX_N];
+ll W;
+
+pair<ll, ll> ps[1 << (MAX_N  / 2)];
+
+void solve() {
+    int n2 = n / 2;
+    for (int i = 0; i < 1 << n2; i++) {
+        ll sw = 0, sv = 0;
+        for (int j = 0; j < n2; j++) {
+            if (i >> j & 1) {
+                sw += w[j];
+                sv += w[j];
+            }
+        }
+        ps[i] = make_pair(sw, sv);
+    }
+
+    sort(ps, ps + (1 << n2));
+    int m = 1;
+
+    for (int i = 1; i < 1 << n2; i++) {
+        if (ps[m - 1].second < ps[i].second) {
+            ps[m++] = ps[i];
+        }
+    }
+
+    ll res = 0;
+    for (int i =  0; i < 1 << (n - n2); i++) {
+        ll sw = 0, sv = 0;
+        for (int j = 0; j < n - n2; j++) {
+            if (i >> j & 1) {
+                sw += w[n2 + j];
+                sv += v[n2 + j];
+            }
+        }
+        if (sw <= W) {
+            ll tv = (lower_bound(ps, ps + m, make_pair(W - sw, INF)) - 1)->second;
+            res = max(res, sv + tv);
+        }
+    }
+
+    cout << res << endl;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> w[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
     cin >> W;
 
+    solve();
+
+    return 0;
 
 
 }
+
