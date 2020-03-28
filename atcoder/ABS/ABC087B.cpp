dfcb5b2ee25e157bec0a9e02ea6a823d9853c8dc
trmr <ryo1.teramura@gmail.com>
2020-03-28 23:45:53 +0900

    ABC087B

--- atcoder/ABS/ABC087B.cpp
+++ atcoder/ABS/ABC087B.cpp
@@ -7,30 +7,45 @@ using ll = long long;
 
 int cnt = 0;
 vector<int> cost = {500, 100, 50};
+vector<int> coin;
+int X;
+
+int rec(int value, int sum) {
+    if (sum == X) {
+        cnt++;
+        return 1;
+    } else if (sum > X) {
+        return 1;
+    }
+
+    if (value == 3) {
+        return 0;
+    }
+
+    REP(i, coin.at(value) + 1) {
+        if (rec(value + 1, sum + (cost.at(value) * i))) {
+            return 0;
+        }
+    }
+
+}
 
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 
     int A, B, C;
-    int X;
     cin >> A >> B >> C >> X;
 
-    int a = X / A;
-    int ma = X % A;
-    int b = ma / B;
-    int mb = ma % B;
-    int c = mb / C;
-    int mc = mb % C;
-
-    if (mc) {
-        cout << 0 << endl;
-        return 0;
+    REP(i, A+1) {
+        REP(j, B+1) {
+            REP(k, C+1) {
+                if ((i * 500 + j * 100 + k * 50) == X) cnt++;
+            }
+        }
     }
 
-    
+    cout << cnt << endl;
 
-
-
-    
+    return 0;
 }
