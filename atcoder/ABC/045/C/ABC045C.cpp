291950de144f4055bfa5a0f28bb85b058200717d
trmr <chalitourist@gmail.com>
2021-01-13 00:04:37 +0900

    ABC045C

---
+++ atcoder/ABC/045/C/ABC045C.cpp
@@ -0,0 +1,106 @@
+#include <cassert>
+#include <cctype>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <csignal>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cinttypes>
+#include <cstdint>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <chrono>
+#include <forward_list>
+#include <initializer_list>
+#include <random>
+#include <regex>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+using ll = long long;
+using Field = vector<vector<ll>>;
+using Graph = vector<vector<ll>>;
+using VI = vector<int>;
+using VL = vector<ll>;
+using VVL = vector<vector<ll>>;
+using VC = vector<char>;
+using PI = pair<int, int>;
+
+#define FOR(i, s, n) for (int i = s; i < (n); i++)
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define ALL(x) x.begin(), x.end()
+
+const long long INF = 1LL<<60;
+const int MOD = 1000000007;
+const double EPS = 0.00000000001;
+
+template<class T> inline bool chmax(T &a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T &a, T b) { if (a > b) { a = b; return true; } return false; }
+
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    ll s;
+
+    cin >> s;
+    int digit = (int)log10(s) + 1;
+    vector<int> S;
+    while (digit != 0) {
+        int d = pow(10, digit - 1);
+        int s1 = s / d;
+        int s2 = s % d;
+        S.push_back(s1);
+        s = s2;
+        digit--;
+    }
+    ll sum = 0;
+
+
+    for (int i = 0; i < 1 << (S.size() - 1); i++) {
+        bitset<10> bs(i);
+        ll tmps = S[0];
+        for (int j = 0; j < S.size() - 1; j++) {
+            if (bs.test(j)) {
+                sum += tmps;
+                tmps = S[j + 1];
+            } else {
+                tmps = tmps * 10 + S[j + 1];
+            }
+        }
+        sum += tmps;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
